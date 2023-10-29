fa2ab7ed3cb9ec65a97af6d68fae9ec3d8d9a545
printfCAT <cliffomanyo@gmail.com>
2023-10-29 18:00:42 +0300

    task 1 fix

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -2,9 +2,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: {
-    main: path.resolve(__dirname, './js/dashboard_main.js'),
-  },
+  entry: './js/dashboard_main.js',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
