b2f430c9ba5ddcd1642efee646cb2e38607b8bbe
printfCAT <cliffomanyo@gmail.com>
2023-11-08 19:43:19 +0300

    reusable comments

---
+++ 0x03-components/task_3/dashboard/src/BodySection/BodySection.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './BodySection.css';
+
+class BodySection extends Component {
+
+    render() {
+        const { children, title } = this.props;
+        return (
+            <div className='bodySection'>
+                <h2>{title}</h2>
+                {children}
+            </div>
+        );
+    }
+}
+
+BodySection.defaultProps = {
+    title: '',
+};
+
+BodySection.propTypes = {
+    title: PropTypes.string,
+};
+
+export default BodySection;
