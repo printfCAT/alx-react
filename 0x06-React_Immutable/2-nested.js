9f37ef1064eea80348b8c7a9027613f6ca0ab1a8
printfCAT <cliffomanyo@gmail.com>
2023-11-15 13:30:17 +0300

    task 2 fix

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
-import Map from 'immutable';
+import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-  const newObject = Map(object);
+  const newObject = fromJS(object);
   return newObject.getIn(array);
 }
