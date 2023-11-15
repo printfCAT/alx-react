b6441df2da69e99d79cf51ab383ddc8363840a7d
printfCAT <cliffomanyo@gmail.com>
2023-11-15 19:42:59 +0300

    nested merge

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,8 +1,8 @@
 /* eslint-disable linebreak-style */
-import { fromJS } from 'immutable';
+import { Map } from 'immutable';
 
 export default function mergeDeeplyElements(page1, page2) {
-  const newPage1 = fromJS(page1);
-  const newPage2 = fromJS(page2);
+  const newPage1 = Map(page1);
+  const newPage2 = Map(page2);
   return newPage1.mergeDeep(newPage2);
 }
