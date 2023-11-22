1752a4ae00afebc73af58e52ed9520a8667e4268
printfCAT <cliffomanyo@gmail.com>
2023-11-22 14:41:33 +0300

    read data from JSON

---
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -0,0 +1,5 @@
+import notificationsData from '../notifications.json';
+
+export default function getAllNotificationsByUser(userId) {
+    return notificationsData.filter((notification) => notification.author.id === userId).map((notification) => notification.context);
+}
