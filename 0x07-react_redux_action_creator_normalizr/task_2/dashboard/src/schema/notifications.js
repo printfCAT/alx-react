15393d73420259fcb47e390c95d3916d5b52f083
printfCAT <cliffomanyo@gmail.com>
2023-11-22 18:32:43 +0300

    filter a normalized schema

---
+++ 0x07-react_redux_action_creator_normalizr/task_2/dashboard/src/schema/notifications.js
@@ -0,0 +1,25 @@
+import { normalize, schema } from 'normalizr';
+import notificationsData from '../../notifications.json';
+
+const user = new schema.Entity('users');
+const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
+const notification = new schema.Entity('notifications', {
+    author: user,
+    context: message,
+});
+
+export const normalizedData = normalize(notificationsData, [notification]);
+
+export default function getAllNotificationsByUser(userId) {
+    const output = [];
+    const notification = normalizedData.entities.notifications;
+    const message = normalizedData.entities.messages;
+
+    for (const id in notification) {
+        if (notification[id].author === userId) {
+            output.push(message[notification[id].context]);
+        }
+    }
+
+    return output;
+}
